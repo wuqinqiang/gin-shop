397e4b200a844701d2f459353edb39299e3ceaf3
吴亲强 <wuqq@weipaitang.com>
2020-12-16 23:28:47 +0800

    【add category handle】

--- handle/category.go
+++ handle/category.go
@@ -1,1 +1,24 @@
 package handle
+
+type Tag struct {
+}
+
+func (tag *Tag) Create() {
+
+}
+
+func (tag *Tag) Update() {
+
+}
+
+func (tag *Tag) Delete() {
+
+}
+
+func (tag *Tag) show() {
+
+}
+
+func (tag *Tag) List() {
+
+}
