3a0bc6bf2f6102c84c3f22b41b3b8c8460715670
吴亲强 <wuqq@weipaitang.com>
2020-12-16 23:23:24 +0800

    【add dig】

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/wuqinqiang/gin-shop/routes"
+	"go.uber.org/dig"
 )
+
+func initContainer() *dig.Container {
+	d := dig.New()
+	return d
+}
 
 func main() {
 	r := gin.Default()
