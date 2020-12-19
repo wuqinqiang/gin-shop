e57bb9ff2101a162a000ba2505bc0acf06e97ab4
吴亲强 <wuqq@weipaitang.com>
2020-12-19 23:29:15 +0800

    【remove dig】

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/wuqinqiang/gin-shop/routes"
-	"go.uber.org/dig"
 )
-
-func initContainer() *dig.Container {
-	d := dig.New()
-	return d
-}
 
 func main() {
 	r := gin.Default()
